e42ca79e236751fcd28d838daa790e737db69f56
Moiz Ahmad <moiznahmad@gmail.com>
2021-08-14 13:45:54 -0700

    initial commit with user auth

---
+++ backend/routes/api/index.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
+
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    })
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+router.get('/restore-user', restoreUser, (req, res) => {
+    return res.json(req.user);
+});
+
+router.get('/require-auth', requireAuth, (req, res) => {
+    return res.json(req.user);
+})
+
+module.exports = router;
